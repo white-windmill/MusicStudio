1ad582b689e6d9a068157eb93c2b30f14a5e1fd4
Ying Ruiqi <3139992043@qq.com>
2022-05-12 18:57:09 +0800

    增加api调用

---
+++ music_studio/lib/common/api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart' as crypto;
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  static final String url = "http://124.220.169.238:8000";
+
+  static String md5(String plain) {
+    var content = new Utf8Encoder().convert(plain);
+    var md5 = crypto.md5;
+    var digest = md5.convert(content);
+    return hex.encode(digest.bytes);
+  }
+
+  static Future<int> check(String username, BuildContext context) async {
+    //json
+    var url = Uri.parse(Api.url + '/user/check?username=' + username);
+    var response = await http.post(url);
+    print('Response body: ${response.body}');
+    if (response.body == '1')
+      return 1;
+    else
+      return 0;
+  }
+}
